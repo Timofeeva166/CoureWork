f09751dbfae2ad1748140c0f8062910b0b9cc49e
Julia Timofeeva <J-Timofeeva166@yandex.ru>
2023-11-14 15:36:46 +0300

    PostgreSQL added

--- src/test/java/ru/netology/data/SQLHelper.java
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -15,7 +15,7 @@ public class SQLHelper {
     }
 
     private static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass");
+        return DriverManager.getConnection(System.getProperty("db.url"), "app", "pass");
     }
 
 
